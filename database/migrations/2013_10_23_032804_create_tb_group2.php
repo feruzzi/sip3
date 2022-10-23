0b721ae0c988aa0b361393f94ea008160d841069
Feruzzi Graha <feruzzig@gmail.com>
2022-10-23 12:49:01 +0700

    add table group1 and group2

---
+++ database/migrations/2013_10_23_032804_create_tb_group2.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group2', function (Blueprint $table) {
+            $table->id();
+            $table->string('group2_id')->unique();
+            $table->string('group2_name');
+            $table->string('group2_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group2');
+    }
+};
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,12 @@ return new class extends Migration
             $table->string('username')->unique();
             $table->string('name');
             $table->string('password');
-            $table->string('group1'); //umum
-            $table->string('group2'); //detailed
+            $table->string('group1')->index(); //umum
+            $table->string('group2')->index(); //detailed
             $table->string('level'); // 1 => admin, 0=>siswa
             $table->string('status'); // 1 => aktif, 0=>nonaktif
+            $table->foreign('group1')->references('group1_id')->on('group1')->restrictOnDelete()->restrictOnUpdate();
+            $table->foreign('group2')->references('group2_id')->on('group2')->restrictOnDelete()->restrictOnUpdate();
             $table->timestamps();
         });
     }
